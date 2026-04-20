a794aad9aa8ddb29c6be834480fb6c5739ab9a8e
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-04-19 22:38:55 -0300

    chore (#104): liberação total do cors

--- projeto/src/main/java/com/api/projeto/controller/ProjetoController.java
+++ projeto/src/main/java/com/api/projeto/controller/ProjetoController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins ="http://localhost:5173")
+@CrossOrigin(origins ="*")
 @RestController
 @RequestMapping("/projetos")
 @RequiredArgsConstructor
